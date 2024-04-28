fd48b914214d34f1d8c752e878e87fef59286497
George Rickus <grickus@berkeley.edu>
2024-04-28 03:20:57 -0700

    Adding feature to waiting room where only the party leader can start the match once an opponent has joined

--- frontend/src/components/WaitingRoom.jsx
+++ frontend/src/components/WaitingRoom.jsx
@@ -10,16 +10,18 @@ const WaitingRoom = () => {
   const [ matchState, setMatchState ] = useState({_id: "", isOpen:false, players:[], solution:[], baseBoard:[]});
   const { userName, setUserName } = useContext(UsernameContext);
   const [ opponentName, setOpponentName] = useState("...");
-  // const [ matchStateChanged, setMatchStateChanged ] = useState(0);
+  const [ pageTitle, setPageTitle] = useState("Waiting for Opponent")
 
   useEffect(()=>{
     socket.on('update-game', match=>{
       setMatchState(match);
       if (match.players.length > 1) {
-        if (match.players[0].username == userName) {
+        if (match.players[0].socketid === socket.id) {
           setOpponentName(match.players[1].username);
+          setPageTitle("Please Start the Match");
         } else {
           setOpponentName(match.players[0].username);
+          setPageTitle("Waiting for Party Leader");
         }
       } 
     });
@@ -29,18 +31,21 @@ const WaitingRoom = () => {
     <>
       <div className="grow flex flex-col justify-center items-center gap-16">
         <h1 className="text-8xl italic font-bold text-center">
-          Waiting for Opponent
+          {pageTitle}
         </h1>
         <h2 className="text-2xl font-bold">Room ID: {matchState._id}</h2>
         <div className="w-2/3 flex flex-col gap-8 md:flex-row md:gap-0 md:justify-evenly">
           <Combatant number={1} username={userName} />
           <Combatant number={2} username={opponentName} />
         </div>
-        <div className="w-2/3 max-w-[900px]">
-          <Link to={`/game/${matchState._id}`}>
-            <Button className="w-full">Start</Button>
-          </Link>
-        </div>
+
+        {pageTitle=="Please Start the Match" && 
+          <div className="w-2/3 max-w-[900px]">
+            <Link to={`/game/${matchState._id}`}>
+              <Button className="w-full">Start</Button>
+            </Link>
+          </div>
+        }
       </div>
     </>
   );
